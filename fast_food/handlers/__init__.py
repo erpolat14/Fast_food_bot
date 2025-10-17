0e7923ebd95b20b4f9a1f0fced216abdac11eee1
Erpolat <qaraqalpaqerpolat@gmail.com>
2025-10-17 17:06:45 +0500

    Add fast food bot files

--- fast_food/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .start import register_start_handlers
-from .order import register_order_handlers
-
-def register_handlers(dp):
-    register_start_handlers(dp)
-    register_order_handlers(dp)
